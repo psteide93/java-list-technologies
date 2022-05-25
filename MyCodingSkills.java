1f578978e41d33b1e278de429c4fce30ad15cded
Parnell <psteide@ford.com>
2022-05-25 06:54:54 -0400

    Fixed alignment

--- MyCodingSkills.java
+++ MyCodingSkills.java
@@ -1,7 +1,7 @@
-public class MyCodingSkills{
-    public static void main(String[] args){
-        String[] technologies = {"CLI", "Git", "HTML", "CSS", "Javascript", "Angular", "Java", "Typescript"};
-        for(int index =0; index < technologies.length; index++){
+public class MyCodingSkills {
+    public static void main(String[] args) {
+        String[] technologies = { "CLI", "Git", "HTML", "CSS", "Javascript", "Angular", "Java", "Typescript" };
+        for (int index = 0; index < technologies.length; index++) {
             System.out.println("I now understand how to program using " + technologies[index]);
         }
     }
